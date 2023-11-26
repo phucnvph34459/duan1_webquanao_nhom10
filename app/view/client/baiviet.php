79de8ab01e92bd36600757d660b642697aab7584
ducdeptraikoaiso2 <122664189+ducdeptraikoaiso2@users.noreply.github.com>
2023-11-26 09:37:20 +0700

    main

---
+++ app/view/client/baiviet.php
@@ -0,0 +1,101 @@
+   <!--== Start Blog Area Wrapper ==-->
+   <section class="blog-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 m-auto">
+            <div class="section-title text-center">
+              <h2 class="title">Bài viết</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="swiper-container swiper-slide-gap post-slider-container">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <!--== Start Blog Item ==-->
+                  <div class="post-item">
+                    <div class="inner-content">
+                      <div class="thumb">
+                        <a href="single-blog.html">
+                          <img class="w-100" src="assets/img/blog/1.jpg" alt="Image-HasTech">
+                        </a>
+                      </div>
+                      <div class="content">
+                        <ul class="meta">
+                          <li><span>post by: </span><a class="author" href="blog.html">Julip</a></li>
+                          <li><span> - Jan, 25 , 21</span></li>
+                        </ul>
+                        <h4 class="title"><a href="single-blog.html">Hot Summer Fashion for Women new collections arrives</a></h4>
+                        <p>Lorem ipsum dolor sit amet, consectet adipi elit, sed do eius tempor incididunt ut labore gthydolore magna aliqua...</p>
+                        <a class="btn-theme" href="single-blog.html">Read more</a>
+                      </div>
+                    </div>
+                  </div>
+                  <!--== End Blog Item ==-->
+                </div>
+                <div class="swiper-slide">
+                  <!--== Start Blog Item ==-->
+                  <div class="post-item">
+                    <div class="inner-content">
+                      <div class="thumb">
+                        <a href="single-blog.html">
+                          <img class="w-100" src="assets/img/blog/2.jpg" alt="Image-HasTech">
+                        </a>
+                      </div>
+                      <div class="content">
+                        <ul class="meta">
+                          <li><span>post by: </span><a class="author" href="blog.html">Morem</a></li>
+                          <li><span> - Jan, 24 , 23</span></li>
+                        </ul>
+                        <h4 class="title"><a href="single-blog.html">But I must explain to you how all this mistaken ideal</a></h4>
+                        <p>Lorem ipsum dolor sit amet, consectet adipi elit, sed do eius tempor incididunt ut labore gthydolore magna aliqua...</p>
+                        <a class="btn-theme" href="single-blog.html">Read more</a>
+                      </div>
+                    </div>
+                  </div>
+                  <!--== End Blog Item ==-->
+                </div>
+                <div class="swiper-slide">
+                  <!--== Start Blog Item ==-->
+                  <div class="post-item">
+                    <div class="inner-content">
+                      <div class="thumb">
+                        <a href="single-blog.html">
+                          <img class="w-100" src="assets/img/blog/3.jpg" alt="Image-HasTech">
+                        </a>
+                      </div>
+                      <div class="content">
+                        <ul class="meta">
+                          <li><span>post by: </span><a class="author" href="blog.html">Adapo</a></li>
+                          <li><span> - Jan, 20 , 23</span></li>
+                        </ul>
+                        <h4 class="title"><a href="single-blog.html">My Favorite White Sneakers From Splurge To Save</a></h4>
+                        <p>Lorem ipsum dolor sit amet, consectet adipi elit, sed do eius tempor incididunt ut labore gthydolore magna aliqua...</p>
+                        <a class="btn-theme" href="single-blog.html">Read more</a>
+                      </div>
+                    </div>
+                  </div>
+                  <!--== End Blog Item ==-->
+                </div>
+                <div class="swiper-slide">
+                  <!--== Start Blog Item ==-->
+                  <div class="post-item">
+                    <div class="inner-content">
+                      <div class="thumb">
+                        <a href="single-blog.html">
+                          <img class="w-100" src="assets/img/blog/1.jpg" alt="Image-HasTech">
+                        </a>
+                      </div>
+                      <div class="content">
+                        <ul class="meta">
+                          <li><span>post by: </span><a class="author" href="blog.html">Julip</a></li>
+                          <li><span> - Jan, 25 , 21</span></li>
+                        </ul>
+                        <h4 class="title"><a href="single-blog.html">On the other hand we provide denounce with righteous</a></h4>
+                        <p>Lorem ipsum dolor sit amet, consectet adipi elit, sed do eius tempor incididunt ut labore gthydolore magna aliqua...</p>
+                        <a class="btn-theme" href="single-blog.html">Read more</a>
+                      </div>
+                    </div>
+                  </div>
+                  <!--== End Blog Item ==-->
